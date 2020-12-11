5fc167858fe61eacbc1089b545f91f453f43cd00
Abdi <abditorres71@gmai.com>
2020-12-11 16:02:10 -0700

    login

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const loginForm = document.getElementsByClassName("loginbox");
-const loginButton = document.getElementsByClassName("submit");
 
+const username = document.getElementById('username')
+const password = document.getElementById('password')
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+const div = document.getElementById('loginbox')
 
-    if (username === "1" && password === "2") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+loginbox.add
