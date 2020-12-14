0d36daea28dc33d170aa6d353a369b8b94715af9
Abdi <abditorres71@gmail.com>
2020-12-14 11:25:29 -0700

    login

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 
-const username = document.getElementById('username')
-const password = document.getElementById('password')
+var objPeople = [
+    {
+        username: "security",
+        password: "Messi1011"
+    }
+   
+]
 
-const div = document.getElementById('loginbox')
+function getInfo() {
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
 
-loginbox.add
+    for(i = 0; i < objPeople.length; i++){
+        if(username == objPeople[i].username && password == objPeople[i].password){
+            console.log(username + " is logged in!!!")
+            return
+        } 
+    }
+    console.log("incorrect username or password")
+}
