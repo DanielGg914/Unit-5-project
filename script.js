68e485bc54e684e1263ac7b4a984ab69136c81c6
Sean Zimbelman <Zimbiekitten@gmail.com>
2020-12-15 16:41:36 -0700

    small changes

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var objPeople = [
         username: "odisha",
         password: "westmec1506"
     }
-   
+
 ]
 
 function getInfo() {
     var username = document.getElementById("username").value
     var password = document.getElementById("password").value
 
-    for(i = 0; i < objPeople.length; i++){
-        if(username == objPeople[i].username && password == objPeople[i].password){
-            window.open('hiring.html')
+    for (i = 0; i < objPeople.length; i++) {
+        if (username == objPeople[i].username && password == objPeople[i].password) {
+            window.open('home.html')
             return
-        } 
+        }
     }
     alert("Incorrect Username or Password")
 }
